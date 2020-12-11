8ffc99cc86fb6491d6b01c890448f9ef0b98b284
Richard Ng <richardcrng@gmail.com>
2020-12-11 14:51:49 +0000

    Add placement tests

--- src/tic-tac-toe.spec.ts
+++ src/tic-tac-toe.spec.ts
@@ -16,4 +16,33 @@ describe('Tic Tac Toe game', () => {
       expect(game.getTurnPlayer()).toBe('X')
     })
   })
+
+  describe('Playing', () => {
+    const game = new TicTacToe()
+    
+    it('Can add markers as specified', () => {
+      game.addMarker({ row: 2, col: 2 })
+      expect(game.getBoard()).toEqual([
+        ['', '', ''],
+        ['', 'X', ''],
+        ['', '', '']
+      ])
+    })
+
+    it('Logs out a message when an occupied cell is attempted', () => {
+      console.log = jest.fn();
+      game.addMarker({ row: 2, col: 2 })
+      expect(console.log).toHaveBeenCalledWith("There's already a marker there - try placing somewhere else!");
+    })
+
+    it("Plays an O on the second marker placed", () => {
+      game.addMarker({ row: 2, col: 3 })
+      expect(game.getBoard()).toEqual([
+        ['', '', ''],
+        ['', 'X', 'O'],
+        ['', '', '']
+      ])
+    })
+
+  })
 })
